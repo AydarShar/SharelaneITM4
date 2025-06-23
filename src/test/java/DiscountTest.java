639c23b01bc345868a89d7f5f8c2a21e3d003e23
Aydar Sharafetdin <aydar141189@gmail.com>
2025-06-23 09:48:09 +0300

    updated DiscountTest

--- src/test/java/DiscountTest.java
+++ src/test/java/DiscountTest.java
@@ -1,66 +1,47 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
+import java.util.HashMap;
 
 public class DiscountTest {
 
-    @Test
-    public void checkDiscount0() {
-        SoftAssert softAssert = new SoftAssert();
-        WebDriver driver = new ChromeDriver();
+    WebDriver driver;
+    SoftAssert softAssert;
+
+    @BeforeMethod
+    public void setup() {
+        softAssert = new SoftAssert();
+        ChromeOptions options = new ChromeOptions();
+        HashMap<String, Object> chromePrefs = new HashMap<>();
+        chromePrefs.put("credentials_enable_service", false);
+        chromePrefs.put("profile.password_manager_enabled", false);
+        options.setExperimentalOption("prefs", chromePrefs);
+        options.addArguments("--incognito");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-popup-blocking");
+        options.addArguments("--disable-infobars");
+        driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-        driver.get("https://sharelane.com/cgi-bin/register.py?page=2&zip_code=11111&first_name=Michael&" +
-                "last_name=Jordan&email=MJ%40gmail.com&password1=Magic&password2=Magic");
-        String email = driver.findElement(By.xpath("//td[text()='Email']/following-sibling::td[1]/b")).getText();
-        driver.get("https://sharelane.com/cgi-bin/main.py");
-        driver.findElement(By.name("email")).sendKeys(email);
-        driver.findElement(By.name("password")).sendKeys("1111");
-        driver.findElement(By.cssSelector("[value=Login]")).click();
-        driver.get("https://sharelane.com/cgi-bin/add_to_cart.py?book_id=6");
-        driver.get("https://sharelane.com/cgi-bin/shopping_cart.py");
-        driver.findElement(By.name("q")).clear();
-        driver.findElement(By.name("q")).sendKeys("19");
-        driver.findElement(By.cssSelector("[value=Update]")).click();
-        softAssert.assertEquals(driver.findElement(By.xpath("//td[text()='10.00']/following-sibling::td[1]/p/b")).getText(), "0");
-        softAssert.assertEquals(driver.findElement(By.xpath("//td[text()='10.00']/following-sibling::td[2]")).getText(), "0");
-        softAssert.assertEquals(driver.findElement(By.xpath("//td[text()='10.00']/following-sibling::td[3]")).getText(), "190");
-        driver.quit();
-        softAssert.assertAll();
     }
 
-    @Test
-    public void checkDiscount2() {
-        SoftAssert softAssert = new SoftAssert();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-        driver.get("https://sharelane.com/cgi-bin/register.py?page=2&zip_code=11111&first_name=Michael&" +
-                "last_name=Jordan&email=MJ%40gmail.com&password1=Magic&password2=Magic");
-        String email = driver.findElement(By.xpath("//td[text()='Email']/following-sibling::td[1]/b")).getText();
-        driver.get("https://sharelane.com/cgi-bin/main.py");
-        driver.findElement(By.name("email")).sendKeys(email);
-        driver.findElement(By.name("password")).sendKeys("1111");
-        driver.findElement(By.cssSelector("[value=Login]")).click();
-        driver.get("https://sharelane.com/cgi-bin/add_to_cart.py?book_id=6");
-        driver.get("https://sharelane.com/cgi-bin/shopping_cart.py");
-        driver.findElement(By.name("q")).clear();
-        driver.findElement(By.name("q")).sendKeys("49");
-        driver.findElement(By.cssSelector("[value=Update]")).click();
-        softAssert.assertEquals(driver.findElement(By.xpath("//td[text()='10.00']/following-sibling::td[1]/p/b")).getText(), "2");
-        softAssert.assertEquals(driver.findElement(By.xpath("//td[text()='10.00']/following-sibling::td[2]")).getText(), "9.8");
-        softAssert.assertEquals(driver.findElement(By.xpath("//td[text()='10.00']/following-sibling::td[3]")).getText(), "480.20");
-        driver.quit();
-        softAssert.assertAll();
+    @DataProvider(name = "discount")
+    public Object[][] provideDiscountData() {
+        return new Object[][] {
+                {"19", "0", "0", "190"},
+                {"49", "2", "9.8", "480.20"},
+                {"50", "3", "15", "485"}
+        };
     }
 
-    @Test
-    public void checkDiscount3() {
-        SoftAssert softAssert = new SoftAssert();
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+    @Test(dataProvider = "discount")
+    public void checkDiscount0(String booksQuantity, String discountPercent, String discountUsd, String total) {
         driver.get("https://sharelane.com/cgi-bin/register.py?page=2&zip_code=11111&first_name=Michael&" +
                 "last_name=Jordan&email=MJ%40gmail.com&password1=Magic&password2=Magic");
         String email = driver.findElement(By.xpath("//td[text()='Email']/following-sibling::td[1]/b")).getText();
@@ -71,11 +52,11 @@ public class DiscountTest {
         driver.get("https://sharelane.com/cgi-bin/add_to_cart.py?book_id=6");
         driver.get("https://sharelane.com/cgi-bin/shopping_cart.py");
         driver.findElement(By.name("q")).clear();
-        driver.findElement(By.name("q")).sendKeys("50");
+        driver.findElement(By.name("q")).sendKeys(booksQuantity);
         driver.findElement(By.cssSelector("[value=Update]")).click();
-        softAssert.assertEquals(driver.findElement(By.xpath("//td[text()='10.00']/following-sibling::td[1]/p/b")).getText(), "3");
-        softAssert.assertEquals(driver.findElement(By.xpath("//td[text()='10.00']/following-sibling::td[2]")).getText(), "15");
-        softAssert.assertEquals(driver.findElement(By.xpath("//td[text()='10.00']/following-sibling::td[3]")).getText(), "485");
+        softAssert.assertEquals(driver.findElement(By.xpath("//td[text()='10.00']/following-sibling::td[1]/p/b")).getText(), discountPercent);
+        softAssert.assertEquals(driver.findElement(By.xpath("//td[text()='10.00']/following-sibling::td[2]")).getText(), discountUsd);
+        softAssert.assertEquals(driver.findElement(By.xpath("//td[text()='10.00']/following-sibling::td[3]")).getText(), total);
         driver.quit();
         softAssert.assertAll();
     }
